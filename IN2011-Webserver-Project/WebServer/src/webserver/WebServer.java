0ab0280e3d6e261004437c46a1b58d853e5e789b
Akash Goswami <akashneil96@gmail.com>
2017-03-29 16:00:38 +0100

    Part 2 Completed

--- IN2011-Webserver-Project/WebServer/src/webserver/WebServer.java
+++ IN2011-Webserver-Project/WebServer/src/webserver/WebServer.java
@@ -10,6 +10,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
 import java.nio.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 import org.apache.http.client.utils.DateUtils;
 
@@ -30,30 +34,87 @@ public class WebServer {
                 Socket conn = serverSock.accept();
                 // get the input stream for receiving data from the client
                 InputStream is = conn.getInputStream();
-                // read the request message (but ignore it for now)
-                String method = "";
-                try {
-                    Request req = Request.parse(is);
-                    method = req.getMethod();
-                } catch (MessageFormatException ex) {}
-                
-                // get the output stream for sending data to the client
-                OutputStream os = conn.getOutputStream();
-                
-                // send a response
-                Response msg;
-                
-                if(method.startsWith("GET")){
-                    msg = new Response(200);
+
+                   try{
+                    // read the request message 
+                       
+                    Request req = Request.parse(is);      
+                    String method = req.getMethod();
+                   
+                    // get the output stream for sending data to the client
+                    try{
+                    OutputStream os = conn.getOutputStream();
+                    Response msg;
+     
+                    if(method.startsWith("GET")){
+                        String uri = req.getURI();
+                        String ps = rootDir + uri;
+                        Path myPath = Paths.get(ps).toAbsolutePath().normalize();
+                        
+                        try{
+                        InputStream input = Files.newInputStream(myPath);
+                        }catch(Exception e){
+                            msg = new Response(404); // Not found
+                            msg.write(os);
+                            conn.close();
+                            return;
+                        }
+                        
+                        System.out.println( "exists:"  + Files.exists(myPath));
+                        System.out.println( "can read:" + Files.isReadable(myPath));
+                        System.out.println( "can execute:" + Files.isExecutable(myPath) );
+                        System.out.println( "can write:" + Files.isWritable(myPath));
+                        
+                        if(Files.exists(myPath) && Files.isReadable(myPath) && Files.isExecutable(myPath) && Files.isWritable(myPath)){
+                            
+                        if(!req.getVersion().equals(in2011.http.HTTPMessage.DEFAULT_HTTP_VERSION)){
+                            
+                            msg = new Response(505);
+                            msg.write(os);
+                            conn.close();
+                            return;
+                        }
+
+                        BasicFileAttributes attr = Files.readAttributes(myPath, BasicFileAttributes.class);
+                        System.out.println(!String.valueOf(Files.getLastModifiedTime(myPath)).equals(attr.creationTime()));
+                        System.out.println("Creation Time" + attr.creationTime());
+                        System.out.println("Modified" + Files.getLastModifiedTime(myPath));
+
+                        if(!String.valueOf(Files.getLastModifiedTime(myPath)).equals(attr.creationTime())){
+                            msg = new Response(304);
+                            msg.write(os); 
+                            conn.close();
+                            return;
+                        } 
+     
+                        msg = new Response(200); // OKAY
+                        msg.write(os);
+    
+                        byte[] data = Files.readAllBytes(myPath);                    
+                        os.write(data);  
+                        
+                        } else {
+                            msg = new Response(403); // Forbidden
+                            msg.write(os);
+                        }
+                        
+                   } else {
+                        msg = new Response(501); // Not implemented.
+                        msg.write(os);
+                    }
+                    }catch(Exception e){
+                        OutputStream os = conn.getOutputStream();
+                        Response msg;
+                        msg = new Response(500);
+                        msg.write(os);
+                    }
+
+                } catch (MessageFormatException ex) {
+                    OutputStream os = conn.getOutputStream();
+                    Response msg;
+                    msg = new Response(400); // Internal server error
                     msg.write(os);
-                    os.write("GET".getBytes());  
-                } else {
-                    
                 }
-                
-                os.write("Hello".getBytes());
-              
-                // close the connection
                 conn.close();
             }
     }
